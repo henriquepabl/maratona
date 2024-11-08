3a251ac02db970315dd1d97efb6e34fc4d846715
henriquepabl <108827693+henriquepabl@users.noreply.github.com>
2024-11-08 13:51:24 -0300

    Update balanced round.c

--- balanced round.c
+++ balanced round.c
@@ -1,55 +1,82 @@
 #include <stdio.h>
-#include <math.h>
+#define MAX 200000
 
-void insertionsort(int n,long long vet[]);
-
+void mergesort(long int vet[MAX], long int aux[MAX],int min,int max);
+void merge(long int vet[MAX],long int aux[MAX],int min,int mid,int max);
 int main(){
 
-    int testes,n,k,ans = 0;
-    long long vet[10000];
-    scanf("%d\n",&testes);
+    int t;
+    long int maxdif;;
+    int n;
+    int ans;
+    long int array[MAX];
+    long int aux[MAX];
 
-    while(testes--){
-        scanf("%d %d\n",&n,&k);
+    scanf("%d",&t);
+    
+    while(t--){
+        ans = 1;
+        int sqfinal = 0;
+        scanf("%d %ld",&n,&maxdif);
         for(int i = 0; i < n;i++){
-            scanf("%lld ",&vet[i]);
+            scanf("%ld",&array[i]);
         }
-        insertionsort(n,vet);
-        int aux = 0;
-        ans = 0;
-        for(int i = 0; i < n -1;i++){
-            if(fabs(vet[i] - vet[i+1]) <= k){
-                ans++;
-            }
-            else{
-                if(ans > aux){
-                    aux = ans;
+        if(n <= 1) sqfinal = 0;
+        else{
+            mergesort(array,aux,0,n-1);    
+            for(int i = 0; i < n-1;i++){
+                if(array[i+1] - array[i] <= maxdif){
+                    ans += 1;
                 }
-                ans = 0;
+                else{
+                    if(ans > sqfinal){
+                        sqfinal = ans;
+                    }
+                    ans = 1;
+                }
             }
         }
-        if(ans > aux){
-            aux = ans;
+        if(ans > sqfinal){
+            sqfinal = ans;
         }
-        printf("%d\n",n-(aux+1));
-        
+        printf("%d\n",n-sqfinal);
+
     }
+    
+ 
     return(0);
 }
+void mergesort(long int vet[MAX],long int aux[MAX],int min,int max){
 
-
-void insertionsort(int n,long long vet[]){
-    long long aux = 0;
-    for(int i = 1;i < n-1;i++){
-        aux = vet[i];
-        int j = i - 1;
-
-        while(j >= 0 && vet[j+1] > aux){
-            vet[j+1] = vet[j];
-            j--;
-        }
-        vet[j+1] = aux;
-
+    if(max <= min){
+        return;
     }
+    int mid = min + ((max - min)/2);
+    mergesort(vet,aux,min,mid);
+    mergesort(vet,aux,mid+1,max);
+    merge(vet,aux,min,mid,max);
 
+}
+void merge(long int vet[MAX],long int aux[MAX],int min,int mid,int max){
+    int i = min;
+    int j = mid+1;
+
+    for(int k = min; k <= max;k++){
+        aux[k] = vet[k];
+    }
+    for(int k = min; k <= max;k++){
+
+        if(i > mid){
+            vet[k] = aux[j++];
+        }
+        else if(j > max){
+            vet[k] = aux[i++];
+        }
+        else if(aux[i] > aux[j]){
+            vet[k] = aux[j++];
+        }
+        else{
+            vet[k] = aux[i++];
+        }
+    }
 }
